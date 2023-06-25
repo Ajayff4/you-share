4151b87137378c76de1bd6a0047214763b0f754c
ajayff4 <ajayff4@gmail.com>
2023-06-26 04:09:36 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/you-share",
   plugins: [react()],
-  base: "/you-share/",
-  server: {
-    port: 3000,
-    open: true,
-  }
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  // }
 })
